072fb47b2d8fbf8f7df612361ebcdb3ace2bd7f3
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-04-19 05:38:12 +0200

    Modify codaxy style include

--- app/Application.js
+++ app/Application.js
@@ -6,7 +6,7 @@
      * Just a customised generic app entry point
      */
     Ext.define('MapHiveHosted.Application', {
-        extend: 'mh.Application',
+        extend: 'mh.Application'
 
         //here goes a custom app launcher class. If not overridden, it will default to mh.AppLauncher
         //appLauncher: ''
