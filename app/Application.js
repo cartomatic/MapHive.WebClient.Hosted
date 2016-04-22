4a46c16254de6b9802938a27fdcd8d74340f4704
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-04-22 11:42:39 +0200

    add default token

--- app/Application.js
+++ app/Application.js
@@ -12,6 +12,8 @@
             'MapHiveHosted.AppLauncher'
         ],
 
+        defaultToken : 'dashboard',
+
         //here goes a custom app launcher class. If not overridden, it will default to mh.AppLauncher
         //Note: in this case MapHiveHosted.AppLauncher just inherits from mh.AppLauncher and is used instead as
         //this is needed  so the proper azzurra scss is brought in!
